528c6f68ef54bfd1f2fd3913de11adf48d808f8d
zsg179 <18977330642@163.com>
2021-09-18 11:27:36 +0800

    first commit

---
+++ src/main/java/com/example/gittest/GitTest.java
@@ -0,0 +1,13 @@
+package com.example.gittest;
+
+/**
+ * @Auther :朱树广
+ * @Date :2021/9/18
+ * @Description :com.example.gittest
+ * @Version :1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}

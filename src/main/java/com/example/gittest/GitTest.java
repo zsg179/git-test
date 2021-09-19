4c4c73e65ce605e1831eb65f4c8905d21c389473
zsg179 <51732714+zsg179@users.noreply.github.com>
2021-09-19 16:57:18 +0800

    Update GitTest.java

--- src/main/java/com/example/gittest/GitTest.java
+++ src/main/java/com/example/gittest/GitTest.java
@@ -8,13 +8,6 @@ package com.example.gittest;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git2");
-        System.out.println("hello git3");
-        System.out.println("hello git4");
-        System.out.println("master test");
-        System.out.println("hot-fix test");
-        System.out.println("push test");
-        System.out.println("pull test");
+        System.out.println("test");
     }
 }
